baaa7bfc00d7e0e4ddf770def45cbd0d8f8b126a
Mustajab <mustajabali.321@gmail.com>
2023-02-12 15:04:46 +0500

    Second Commit Added Word and charchter counter Added preview functionality Added functionality to convert text to lowercase.

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,12 +7,16 @@ export default function TextForm(props) {
         let newText = text.toUpperCase()
         setText(newText)
     }
+    const handleLcCLick =()=>{
+        let newText = text.toLowerCase()
+        setText(newText)
+    }
 
     const handleOnChange =(event)=>{
         setText(event.target.value)
     }
 
-    const [text, setText] = useState("Enter Text Here");
+    const [text, setText] = useState(" ");
 
     return (
         <>
@@ -21,10 +25,16 @@ export default function TextForm(props) {
             <div className="mb-3">
                 <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
             </div>
-            <button className="btn btn-primary" onClick={handleUPCLick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleUPCLick}>Convert to Uppercase</button>
+            <button className="btn btn-primary mx-2" onClick={handleLcCLick}>Convert to Lowercase</button>
+
         </div>
-        <div className="container">
-            <h1>Your Text Summary</h1>
+        <div className="container my-3">
+            <h2>Your Text Summary</h2>
+            <p>{text.split(" ").length} words, {text.length} charachters</p>
+            <p>{0.008 * text.split(" ").length} minutes read</p>     
+            <h2>Preview</h2>
+            <p>{text}</p>
         </div>
         </>
     )
